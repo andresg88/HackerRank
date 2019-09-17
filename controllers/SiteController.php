b550a3a984810df96c1e67f98479ce837e4cbe0f
Daniel Ciro <taromaciro@gmail.com>
2019-09-16 22:19:14 -0500

    Ultimos cambios realizados

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -61,7 +61,68 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
+        if (Yii::$app->request->post()) {
+            // echo "<pre>";
+            // print_r($_POST['input']);
+            // die;
+            $input = $_POST['input'];
+            $my_file = 'cube.txt';
+            $handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
+            fwrite($handle, $input);
+            $output = $this->cube();
+            return $this->render('index', ['output' => $output]);
+        }
         return $this->render('index');
+    }
+
+    public function cube(){
+        $input = fopen('../web/cube.txt', "r");
+        $t= fgets($input);
+        $cube = [];
+        for($i=0;$i<$t;$i++){
+            $Matriz=fgets($input);
+            $Matriz = explode(" ", $Matriz);
+            $n=$Matriz[0];
+            $m=$Matriz[1];
+            // echo $n." ".$m;  die;
+            $arr = array_fill(0, $n*$n*$n, 0);
+            for($j=0; $j<$m; $j++){
+                $operation=fgets($input);
+                $operation = explode(" ", $operation);
+                if($operation[0]=="UPDATE"){
+                    $arr = $this->update($operation[1],$operation[2],$operation[3],$operation[4],$n,$arr);
+                    // array_push($cube, (int) $arr);
+                }else{
+                    $val = $this->query($operation[1],$operation[2],$operation[3],$operation[4],$operation[5],$operation[6],$n,$arr);
+                    array_push($cube, (int) $val);
+                }
+            }  
+        }
+        return $cube;
+    }
+    
+    public function update($x, $y, $z, $value, $n, $arr){
+        $arr[$this->getIndex($x, $y, $z,$n)] = (int) $value;
+        return $arr;
+    }
+
+    public function query($x1,$y1,$z1,$x2,$y2,$z2,$n,$arr){
+        $arr = (array) $arr;
+        $a = $this->getIndex($x1, $y1, $z1, $n);
+        $b = $this->getIndex($x2, $y2, $z2, $n) + 1;
+        $newArr = array_slice($arr, $a, $b);
+        $aux = array_sum($newArr)."\n";
+        return $aux;
+    }
+
+    public function getIndex($x, $y, $z, $n){
+        $x = (int) $x;
+        $y = (int)  $y;
+        $z = (int)  $z; 
+        $n = (int)  $n;
+        $result = (($n*$n)*($x-1) + ($n*($y-1)) + $z) - 1;
+        // echo $result;  die;
+        return $result;
     }
 
     /**
--- views/site/index.php
+++ views/site/index.php
@@ -18,39 +18,66 @@ $this->title = 'My Yii Application';
     </div>
 
     <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+    <center>
+        Input<br>
+        <?php
+$textarea = '2
+4 5
+UPDATE 2 2 2 4
+QUERY 1 1 1 3 3 3
+UPDATE 1 1 1 23
+QUERY 2 2 2 4 4 4
+QUERY 1 1 1 3 3 3
+2 4
+UPDATE 2 2 2 1
+QUERY 1 1 1 1 1 1
+QUERY 1 1 1 2 2 2
+QUERY 2 2 2 2 2 2';
+            echo Html::textarea('input', $textarea, ['rows' => 13 , 'cols' => 50]); 
+        ?>
+        <br>
+        <div class="form-group">
+            <div class="col-lg-12">
+                <?= Html::a('Cube', Url::to(['site/index']), [
+                        'class'=>'btn btn-primary',
+                        'data' => [
+                            'method' => 'POST', 
+                            'params' => [
+                                'input' => $textarea
+                            ]
+                        ]
+                    ]) 
+                ?>
             </div>
         </div>
+        <br>
+        Output<br>
+        <textarea id='output' rows="7" cols="50">
+<?php 
+if(!empty($output)){
+// echo "<pre>";
+// print_r($output);
+// die;
+$output = implode("\n", $output);
+echo trim($output);
+}
+else{
+echo ltrim("No hay datos aun");
+}
+?>
+        </textarea>
+
+        <br>
+        OutPut Esperado resultado correcto<br>
+        <textarea id='output' rows="7" cols="50">
+4
+4
+27
+0
+1
+1
+        </textarea>
+    </center>
 
     </div>
 </div>
